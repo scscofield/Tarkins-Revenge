9071cbdced8a806458d9f7af177d5456bba58577
stacy19201325 <stacy_19201325@hotmail.com>
2018-12-15 19:39:45 -0600

    [Revamp] Jawa Trader POI

--- MMOCoreORB/bin/scripts/mobile/tarkin_custom/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/tarkin_custom/serverobjects.lua
@@ -5,3 +5,12 @@ includeFile("tarkin_custom/lok/de_droideka.lua")
 includeFile("tarkin_custom/lok/de_ig_assassin_droid.lua")
 includeFile("tarkin_custom/lok/de_s_battle_droid.lua")
 includeFile("tarkin_custom/lok/de_s_battle_droid_boss.lua")
+
+--Jawa POI
+includeFile("tarkin_custom/tatooine/jawa_engineer_poi.lua")
+includeFile("tarkin_custom/tatooine/jawa_poi.lua")
+includeFile("tarkin_custom/tatooine/jawa_protector_poi.lua")
+includeFile("tarkin_custom/tatooine/jawa_thief_poi.lua")
+includeFile("tarkin_custom/tatooine/jawa_healer_poi.lua")
+includeFile("tarkin_custom/tatooine/jawa_warlord_poi.lua")
+includeFile("tarkin_custom/tatooine/jawa_leader_poi.lua")
