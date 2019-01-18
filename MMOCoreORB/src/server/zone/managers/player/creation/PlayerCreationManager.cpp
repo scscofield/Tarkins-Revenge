c0dd3116256bb339495b91c3d1860747a6de8a87
stacy19201325 <stacy_19201325@hotmail.com>
2019-01-17 22:58:18 -0600

    [Eliminated] Group XP modifier and set solo XP modifier to 2.0, removing the need for solo groups

--- MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
+++ MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
@@ -634,7 +634,7 @@ bool PlayerCreationManager::createCharacter(ClientCreateCharacterCallback* callb
    	promptText << endl;
    	promptText << "\\#5bff71Server XP Rates:";
    	promptText << endl;
-   	promptText << "\\#f2f5f9-- 1.5x XP Solo | 2x XP Grouped";
+   	promptText << "\\#f2f5f9-- 2x XP (grouped or solo is the same - we have removed the need for solo groups)";
    	promptText << endl;
    	promptText << endl;
    	promptText << endl;
