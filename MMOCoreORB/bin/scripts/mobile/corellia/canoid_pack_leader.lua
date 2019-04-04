eb4d5d9f6e2c0d16d31819831ac3320b36941c44
stacy <stacy_19201325@hotmail.com>
2019-04-03 20:10:26 -0500

    [Fixed] Canoid hues

--- MMOCoreORB/bin/scripts/mobile/corellia/canoid_pack_leader.lua
+++ MMOCoreORB/bin/scripts/mobile/corellia/canoid_pack_leader.lua
@@ -26,7 +26,7 @@ canoid_pack_leader = Creature:new {
 	diet = CARNIVORE,
 
 	templates = {"object/mobile/canoid_pack_leader.iff"},
-	hues = { 16, 17, 18, 19, 20, 21, 22, 23 },
+	hues = { 2, 3, 13 },
 	scale = 1.25,
 	lootGroups = {},
 	weapons = {},
