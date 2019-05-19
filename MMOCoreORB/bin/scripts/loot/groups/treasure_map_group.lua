2df7c7d9b2e23950784ec50d06e6ba6b89d27591
R. Bassett Jr <alsopeachy@gmail.com>
2019-05-19 09:40:09 -0400

    Treasure Map loot adjustments
    Change-Id: Ib8cc21a339e7ac8bfbef4fc8650b9774c4b52ba7

--- MMOCoreORB/bin/scripts/loot/groups/treasure_map_group.lua
+++ MMOCoreORB/bin/scripts/loot/groups/treasure_map_group.lua
@@ -3,15 +3,14 @@ treasure_map_group = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "armor_all", weight = 1224603},
-		{itemTemplate = "armor_attachments", weight = 306151},
-		{itemTemplate = "clothing_attachments", weight = 306151},
-		{itemTemplate = "color_crystals", weight = 306151},
-		{itemTemplate = "power_crystals", weight = 306151},
-		{itemTemplate = "junk", weight = 4490385},
-		{itemTemplate = "tailor_components", weight = 509151},
-		{itemTemplate = "weapons_all", weight = 1326654},
-		{itemTemplate = "wearables_all", weight = 1224603}
+		{itemTemplate = "armor_all", weight = 1900000},
+		{itemTemplate = "armor_attachments", weight = 750000},
+		{itemTemplate = "clothing_attachments", weight = 750000},
+		{itemTemplate = "color_crystals", weight = 500000},
+		{itemTemplate = "power_crystals", weight = 500000},
+		{itemTemplate = "junk", weight = 1800000},
+		{itemTemplate = "weapons_all", weight = 1900000},
+		{itemTemplate = "wearables_all", weight = 1900000}
 	}
 }
 
