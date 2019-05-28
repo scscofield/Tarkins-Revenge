8884769bef08efa63b3916ddee17db737431910a
stacy19201325 <stacy_19201325@hotmail.com>
2019-05-27 20:16:07 -0500

    [Added] Functionality to Enzyme Refining Tray

--- MMOCoreORB/src/server/zone/objects/tangible/components/CentrifugeTrayMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/CentrifugeTrayMenuComponent.cpp
@@ -1,13 +1,19 @@
 /*
  * CentrifugeTrayMenuComponent.cpp
- * Tarkin's Revenge
- * May 2019
+ *
+ *  Tarkin's Revenge
+ *     May 2019
  */
 
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/objects/player/PlayerObject.h"
+#include "server/zone/objects/scene/SceneObject.h"
 #include "CentrifugeTrayMenuComponent.h"
+#include "server/zone/managers/loot/LootManager.h"
+#include "server/zone/Zone.h"
+#include "server/zone/ZoneServer.h"
 #include "server/zone/packets/object/ObjectMenuResponse.h"
+#include "server/zone/objects/tangible/events/ProcessTrayNotifyAvailableEvent.h"
 
 void CentrifugeTrayMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const {
 
@@ -29,11 +35,20 @@ int CentrifugeTrayMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject
 		return 0;	
 
 	if (selectedID == 115) {	
+		ManagedReference<LootManager*> lootManager = sceneObject->getZone()->getZoneServer()->getLootManager();
+
+		if (lootManager == NULL) {
+			player->sendSystemMessage("Error: LootManager is NULL. Contact an Admin for help.");
+			return 0;
+		}	
 
 		if (!sceneObject->isASubChildOf(player)) {
 			player->sendSystemMessage("Enzyme Refining Tray must be in your inventory to use.");
 			return 0;
 		}	
+
+		if (!sceneObject->isContainerObject())
+			return 0;
 
 		SceneObject* inventory = player->getSlottedObject("inventory");
 
@@ -45,7 +60,65 @@ int CentrifugeTrayMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject
 			return 0;
 		}
 
-		//Populate with tray function	
+		int containerSize = sceneObject->getContainerObjectsSize();
+		if (containerSize > 5){  // Refining tray should only hold five item
+			player->sendSystemMessage("Error: Too many items in centrifuge.");
+			return 0;
+		}
+
+		ManagedReference<SceneObject*> enzymeComponentA = NULL;
+		ManagedReference<SceneObject*> enzymeComponentB = NULL;
+		ManagedReference<SceneObject*> enzymeComponentC = NULL;
+		ManagedReference<SceneObject*> enzymeComponentD = NULL;
+		ManagedReference<SceneObject*> enzymeComponentE = NULL;
+
+		int enzymeElements = 0;
+
+		for (int i = 0; i < sceneObject->getContainerObjectsSize(); i++) {
+
+			SceneObject* enzymeElement = sceneObject->getContainerObject(i);
+
+			if (enzymeElement->getServerObjectCRC() == 0x23CD608F) { // object/tangible/loot/beast/enzyme_3_element_1.iff
+				enzymeComponentA = enzymeElement;
+			} else if (enzymeElement->getServerObjectCRC() == 0xF8DAC818) { // object/tangible/loot/beast/enzyme_3_element_2.iff
+				enzymeComponentB = enzymeElement;
+			} else if (enzymeElement->getServerObjectCRC() == 0xB1D7AF95) { // object/tangible/loot/beast/enzyme_3_element_3.iff
+				enzymeComponentC = enzymeElement;
+			} else if (enzymeElement->getServerObjectCRC() == 0x4A348481) { // object/tangible/loot/beast/enzyme_3_element_4.iff
+				enzymeComponentD = enzymeElement;
+			} else if (enzymeElement->getServerObjectCRC() == 0x339E30C) { // object/tangible/loot/beast/enzyme_3_element_5.iff
+				enzymeComponentE = enzymeElement;
+			}
+				enzymeElements++;
+		}
+
+		if (enzymeElements > 5) {
+			player->sendSystemMessage("Error: Too many enzyme elements in the refining tray.");
+			return 0;
+		} else if (enzymeElements < 5) {
+			player->sendSystemMessage("Error: Not enough enzyme elements in the refining tray.  You need one each of A, B, C, D, and E.");
+			return 0;
+		}
+
+		if (enzymeComponentA == NULL || enzymeComponentB == NULL || enzymeComponentC == NULL || enzymeComponentD == NULL || enzymeComponentE == NULL ) {
+			player->sendSystemMessage("Error: Wrong combination of enzyme elements. You need one each of A, B, C, D, and E.");
+			return 0;
+		}
+
+		// Add a short delay
+		player->sendSystemMessage("Refining five enzyme elements, please wait...");
+		Reference<ProcessTrayNotifyAvailableEvent*> task = new ProcessTrayNotifyAvailableEvent(player, sceneObject); 
+		sceneObject->addPendingTask("processing", task, 3 * 1000); // Delete the tray & contents and create one isomerase element in the task
+
+		// If tray is processing, deny player access to the container
+		if (sceneObject->getPendingTask("processing") != NULL) {
+			ContainerPermissions* itemPerms = sceneObject->getContainerPermissions();
+			itemPerms->setInheritPermissionsFromParent(false);
+			itemPerms->clearDefaultAllowPermission(ContainerPermissions::OPEN);
+			itemPerms->clearDefaultAllowPermission(ContainerPermissions::MOVECONTAINER);
+			itemPerms->setDefaultDenyPermission(ContainerPermissions::OPEN);
+			itemPerms->setDefaultDenyPermission(ContainerPermissions::MOVECONTAINER);
+		}	
 	}
 	
 	return 1;
