fe23c8223a5179648f8db3ed7ff8e861e6aa07d8
stacy19201325 <stacy_19201325@hotmail.com>
2019-08-02 03:32:40 -0500

    [Added] The rest of the NGE houses into the regular loot tables

--- MMOCoreORB/bin/scripts/loot/groups/tarkin_custom/afarathu_cave_rare.lua
+++ MMOCoreORB/bin/scripts/loot/groups/tarkin_custom/afarathu_cave_rare.lua
@@ -3,17 +3,18 @@ afarathu_cave_rare = {
 	minimumLevel = 0,
 	maximumLevel = 0,	
 	lootItems = {
-		{itemTemplate = "pistol_renegade_schematic", weight = 909091},
-		{itemTemplate = "carbine_sfor_schematic", weight = 909091},
-		{itemTemplate = "rifle_fallann_schematic", weight = 909091},
-		{itemTemplate = "oneh_sword_mustafar_bandit_schematic", weight = 909091},
-		{itemTemplate = "lance_gand_shockprod_schematic", weight = 909091},
-		{itemTemplate = "muzzle_pistol_renegade", weight = 909091},
-		{itemTemplate = "coil_carbine_sfor", weight = 909091},
-		{itemTemplate = "barrel_rifle_fallann", weight = 909091},
-		{itemTemplate = "bandit_blade", weight = 909091},
-		{itemTemplate = "shock_prodder", weight = 909091},
-		{itemTemplate = "transthermal_padding", weight = 909090}
+		{itemTemplate = "pistol_renegade_schematic", weight = 833333},
+		{itemTemplate = "carbine_sfor_schematic", weight = 833333},
+		{itemTemplate = "rifle_fallann_schematic", weight = 833333},
+		{itemTemplate = "oneh_sword_mustafar_bandit_schematic", weight = 833333},
+		{itemTemplate = "lance_gand_shockprod_schematic", weight = 833333},
+		{itemTemplate = "muzzle_pistol_renegade", weight = 833333},
+		{itemTemplate = "coil_carbine_sfor", weight = 833333},
+		{itemTemplate = "barrel_rifle_fallann", weight = 833333},
+		{itemTemplate = "bandit_blade", weight = 833333},
+		{itemTemplate = "shock_prodder", weight = 833333},
+		{itemTemplate = "transthermal_padding", weight = 833333},
+		{itemTemplate = "nge_vip_house_scheme", weight = 833337},
 	}
 }
 
--- MMOCoreORB/bin/scripts/loot/groups/tarkin_custom/rogue_corsec_base_rare.lua
+++ MMOCoreORB/bin/scripts/loot/groups/tarkin_custom/rogue_corsec_base_rare.lua
@@ -3,14 +3,15 @@ rogue_corsec_base_rare = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "pistol_alliance_disruptor_schematic", weight = 1250000},
-		{itemTemplate = "carbine_alliance_needler_schematic", weight = 1250000},
-		{itemTemplate = "rifle_alliance_gauss_schematic", weight = 1250000},
-		{itemTemplate = "heavy_lava_cannon_schematic", weight = 1250000},
-		{itemTemplate = "coil_pistol_alliance_disruptor", weight = 1250000},
-		{itemTemplate = "barrel_carbine_alliance_needler", weight = 1250000},
-		{itemTemplate = "barrel_rifle_alliance_gauss", weight = 1250000},
-		{itemTemplate = "barrel_heavy_lava_cannon", weight = 1250000}
+		{itemTemplate = "pistol_alliance_disruptor_schematic", weight = 1111111},
+		{itemTemplate = "carbine_alliance_needler_schematic", weight = 1111111},
+		{itemTemplate = "rifle_alliance_gauss_schematic", weight = 1111111},
+		{itemTemplate = "heavy_lava_cannon_schematic", weight = 1111111},
+		{itemTemplate = "coil_pistol_alliance_disruptor", weight = 1111111},
+		{itemTemplate = "barrel_carbine_alliance_needler", weight = 1111111},
+		{itemTemplate = "barrel_rifle_alliance_gauss", weight = 1111111},
+		{itemTemplate = "barrel_heavy_lava_cannon", weight = 1111111},
+		{itemTemplate = "nge_relaxation_house_scheme", weight = 1111112}
 	}
 }
 
