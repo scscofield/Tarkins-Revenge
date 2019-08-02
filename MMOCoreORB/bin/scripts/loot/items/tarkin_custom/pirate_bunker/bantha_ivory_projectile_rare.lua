8d62131ac3318266465d71f5387133e6d0c466a1
stacy19201325 <stacy_19201325@hotmail.com>
2019-08-02 03:02:18 -0500

    [Fixed] One type of Smuggled Bantha Ivory to use the correct template

--- MMOCoreORB/bin/scripts/loot/items/tarkin_custom/pirate_bunker/bantha_ivory_projectile_rare.lua
+++ MMOCoreORB/bin/scripts/loot/items/tarkin_custom/pirate_bunker/bantha_ivory_projectile_rare.lua
@@ -2,7 +2,7 @@ bantha_ivory_projectile_rare = {
 	minimumLevel = 0,
 	maximumLevel = -1,
 	customObjectName = "Smuggled Bantha Ivory",
-	directObjectTemplate = "object/tangible/component/weapon/acklay_bone_reinforcement_core.iff",
+	directObjectTemplate = "object/tangible/tarkin_custom/statted/projectile_feed_mechanism_enhancement_bantha.iff",
 	craftingValues = {
 		{"maxdamage",110,175,0},
 		{"attackspeed",0,-1,1},
